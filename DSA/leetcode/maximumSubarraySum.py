1932c8f1e17b90e0d071a1e7f3dd8696d966fd11
shinisoba <83575694+shinisoba@users.noreply.github.com>
2025-03-19 09:49:14 +0530

    Update maximumSubarraySum.py

--- DSA/leetcode/maximumSubarraySum.py
+++ DSA/leetcode/maximumSubarraySum.py
@@ -1,4 +1,13 @@
 #Given an integer array nums, find the subarray with the largest sum, and return its sum.
 class Solution(object):
     def maxSubArray(self, nums):
+        maxs = nums[0]
+        S=0
+        for i in range(0,len(nums):
+            s+=nums[i]
+            if s>maxs:
+                maxs = s
+            if s<0:
+                s=0
+        return maxs
       
